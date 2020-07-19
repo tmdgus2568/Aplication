735dc1958c8183ddd571bcc0fac112f1ba56e1bc
홍승현 <hsh@hongseunghyeon-ui-MacBookAir.local>
2020-07-19 13:54:13 +0900

    PhoneBook filtering

--- Portfolio/app/src/main/java/com/example/portfolio/PhoneBook.java
+++ Portfolio/app/src/main/java/com/example/portfolio/PhoneBook.java
@@ -3,6 +3,9 @@ package com.example.portfolio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -15,6 +18,10 @@ public class PhoneBook extends AppCompatActivity {
     @BindView(R.id.phone_list) ListView itemListView;
     @BindView(R.id.total)
     TextView totalText;
+    @BindView(R.id.filter)
+    EditText filterEdit;
+    int listSize = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +30,42 @@ public class PhoneBook extends AppCompatActivity {
         ButterKnife.bind(this);
         PhoneBookAdapter adapter = new PhoneBookAdapter(this, generateItemList());
         itemListView.setAdapter(adapter);
-        totalText.setText("총 "+adapter.getCount()+"개의 연락처");
+        listSize = adapter.getCount();
+        totalText.setText("총 "+listSize+"개의 연락처");
+        filterEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if((s.toString()).isEmpty()) {
+                    itemListView.setAdapter(adapter);
+                    return ;
+                }
+                ArrayList<Item_phone> filter_list = new ArrayList<>();
+                String itemNames[] = getResources().getStringArray(R.array.phone_name);
+                String itemNumber[] = getResources().getStringArray(R.array.phone_number);
+
+                for(int i=0;i<itemNames.length;i++){
+                    if(itemNames[i].indexOf(s.toString()) != -1){
+                        filter_list.add(new Item_phone(itemNames[i],itemNumber[i]));
+                    }
+                    else if (itemNumber[i].indexOf(s.toString()) != -1){
+                        filter_list.add(new Item_phone(itemNames[i],itemNumber[i]));
+                    }
+                }
+
+                PhoneBookAdapter filter_adapter = new PhoneBookAdapter(getApplicationContext(), filter_list);
+                itemListView.setAdapter(filter_adapter);
+            }
+        });
 
     }
 
@@ -34,6 +76,12 @@ public class PhoneBook extends AppCompatActivity {
         for(int i=0;i<itemNames.length;i++){
             list.add(new Item_phone(itemNames[i],itemNumber[i]));
         }
+        return list;
+    }
+
+    private ArrayList<Item_phone> filterItemList(){
+        ArrayList<Item_phone> list = new ArrayList<>();
+
         return list;
     }
 }
