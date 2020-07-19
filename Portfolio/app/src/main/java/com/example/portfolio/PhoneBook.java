a4011169af7f3aff279160042c55759e5798593e
홍승현 <hsh@hongseunghyeon-ui-MacBookAir.local>
2020-07-19 14:03:17 +0900

    PhoneBook filtering

--- Portfolio/app/src/main/java/com/example/portfolio/PhoneBook.java
+++ Portfolio/app/src/main/java/com/example/portfolio/PhoneBook.java
@@ -45,14 +45,18 @@ public class PhoneBook extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
+                // 비어있을 때 원래 list를 보여줌
                 if((s.toString()).isEmpty()) {
                     itemListView.setAdapter(adapter);
                     return ;
                 }
-                ArrayList<Item_phone> filter_list = new ArrayList<>();
+
+                // 검색기능을 위한 새로운 ArrayList 생성
+               ArrayList<Item_phone> filter_list = new ArrayList<>();
                 String itemNames[] = getResources().getStringArray(R.array.phone_name);
                 String itemNumber[] = getResources().getStringArray(R.array.phone_number);
 
+                // EditText에 입력된 값이 포함되어 있다면 filter_list에 값 추가
                 for(int i=0;i<itemNames.length;i++){
                     if(itemNames[i].indexOf(s.toString()) != -1){
                         filter_list.add(new Item_phone(itemNames[i],itemNumber[i]));
@@ -62,8 +66,12 @@ public class PhoneBook extends AppCompatActivity {
                     }
                 }
 
+                // filter_list를 위한 새로운 adapter 생성 및 연결
                 PhoneBookAdapter filter_adapter = new PhoneBookAdapter(getApplicationContext(), filter_list);
                 itemListView.setAdapter(filter_adapter);
+
+
+
             }
         });
 
